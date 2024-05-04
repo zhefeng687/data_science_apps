b8303a0e6a7765ee0ccaed571e27a5c2b6ddd544
zhe <fengzhe424@hotmail.com>
2024-05-03 22:58:34 -0400

    upload a working in progress app

---
+++ soccer_app.py
@@ -0,0 +1,26 @@
+"""
+lxml is the most feature-rich and easy-to-use library 
+for processing XML and HTML in the Python language.
+XML and JSON are used for the storage and transmission of data, 
+while HTML is used to describe how that data is displayed
+
+"""
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pt
+import seaborn as sns 
+
+"""
+A built-in module in Python, the base64 library 
+offers functions for encoding and decoding binary data 
+to and from base64 strings, 
+effectively converting any binary data to plain text. 
+A common encoding method called Base64 converts binary data 
+into a string of printable ASCII characters.
+
+"""
+
+
